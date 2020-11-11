05d71146958a90872ffecc4836d635d8b3d00415
ConardMayes-flatiron <ConradAsatoMayes+flatironschool@gmail.com>
2020-11-10 22:35:16 -0600

    commit tree 32cfce268cfe492bcc89780accb20cbd235bbe07

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,17 @@
 def unsafe?(speed)
-
+    if speed > 60
+        return true
+    elsif speed < 40
+        return true
+    else
+        return false
+    end
 end
 
 
 
 def not_safe?(speed)
+    unsafe?(speed) ? true : false
 	
 end
 	
